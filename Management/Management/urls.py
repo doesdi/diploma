9c803500a7c8c5b73781564187394b0bc38a9a9f
Evgeniy Samoilov <husokka@mail.ru>
2023-04-23 15:56:20 +0300

    third

--- Management/Management/urls.py
+++ Management/Management/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
+from itservice.views import main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main),
 ]
